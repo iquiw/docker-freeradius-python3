8c3ff4508ef5736be22c7582a2dae33ba911f679
Iku Iwasa <iku.iwasa@gmail.com>
2022-09-11 18:00:37 +0900

    Setup GitHub Action to build and push Docker image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,7 @@ generate_python3() {
 			echo "	pass_all_vps = $FREERADIUS_PYTHON3_PASS_ALL_VPS"
 		fi
 		if [ -n "$FREERADIUS_PYTHON3_PASS_ALL_VPS_DICT" ]; then
-			echo "	pass_all_vps = $FREERADIUS_PYTHON3_PASS_ALL_VPS_DICT"
+			echo "	pass_all_vps_dict = $FREERADIUS_PYTHON3_PASS_ALL_VPS_DICT"
 		fi
 		for callback in ${FREERADIUS_PYTHON3_CALLBACKS}; do
 			echo "	mod_$callback = \${.module}"
